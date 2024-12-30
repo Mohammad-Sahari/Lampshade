3246c10ea7e685bc3ced6e374066c12410f0bb75
Mohammad Sahari <mohammadsahari98@outlook.com>
2024-12-30 13:34:04 +0330

    feat : Implement Search and GetDetails Methods from ProductCategoryRepository

--- ShopManagement.Infrastructure.EFCore/Repository/ProductCategoryRepository.cs
+++ ShopManagement.Infrastructure.EFCore/Repository/ProductCategoryRepository.cs
@@ -1,12 +1,72 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Linq.Expressions;
+using ShopManagement.Application.Contracts.ProductCategory;
+using ShopManagement.Domain.ProductCategoryAgg;
 
 namespace ShopManagement.Infrastructure.EFCore.Repository
 {
-    internal class ProductCategoryRepository
+    public class ProductCategoryRepository : IProductCategoryRepository
     {
+        private readonly ShopContext _context;
+
+        public ProductCategoryRepository(ShopContext context)
+        {
+            _context = context;
+        }
+
+        public void Add(ProductCategory entity)
+        {
+            _context.ProductCategories.Add(entity);
+        }
+
+        public ProductCategory Get(long id)
+        {
+            return _context.ProductCategories.Find(id);
+        }
+
+        public List<ProductCategory> GetList()
+        {
+            return _context.ProductCategories.ToList();
+        }
+
+        public bool Exists(Expression<Func<ProductCategory, bool>> expression)
+        {
+            return _context.ProductCategories.Any(expression);
+        }
+
+        public void SaveChanges()
+        {
+            _context.SaveChanges();
+        }
+
+        public EditProductCategory GetDetails(long id)
+        {
+            return _context.ProductCategories.Select(x => new EditProductCategory
+            {
+                Id = x.Id,
+                Name = x.Name,
+                Description = x.Description,
+                Picture = x.Picture,
+                PictureAlt = x.PictureAlt,
+                PictureTitle = x.PictureTitle,
+                Keywords = x.Keywords,
+                MetaDescription = x.MetaDescription,
+                Slug = x.Slug
+            }).FirstOrDefault(x => x.Id == id);
+        }
+
+        public List<ProductCategoryViewModel> Search(ProductCategorySearchModel searchModel)
+        {
+            var query = _context.ProductCategories.Select(x => new ProductCategoryViewModel
+            {
+                Id = x.Id,
+                Name = x.Name,
+                Picture = x.Picture,
+                CreationDate = x.CreationDate.ToString()
+            });
+            if(!string.IsNullOrWhiteSpace(searchModel.Name))
+                query = query.Where(x => x.Name.Contains(searchModel.Name));
+            return query.OrderByDescending(x => x.Id).ToList();
+        }
     }
+
 }
