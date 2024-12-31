9f99453683ab3efc97ce061a508b4ce4615b5e07
Mohammad Sahari <mohammadsahari98@outlook.com>
2024-12-31 17:32:11 +0330

    feat : Implement ProductCategories Section in the Shop Section -LampshadeDb generated. -Search is functional.

---
+++ ServiceHost/Areas/Administration/Pages/Shop/ProductCategories/Index.cshtml.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using ShopManagement.Application.Contracts.ProductCategory;
+
+namespace ServiceHost.Areas.Administration.Pages.Shop.ProductCategories
+{
+    public class IndexModel : PageModel
+    {
+        public List<ProductCategoryViewModel> ProductCategories;
+        public ProductCategorySearchModel searchModel;
+        private readonly IPorductCategoryApplication _productCategoryApplication;
+
+        public IndexModel(IPorductCategoryApplication productCategoryApplication)
+        {
+            _productCategoryApplication = productCategoryApplication;
+        }
+
+        public void OnGet(ProductCategorySearchModel searchModel)
+        {
+            ProductCategories = _productCategoryApplication.Search(searchModel);
+        }
+    }
+}
