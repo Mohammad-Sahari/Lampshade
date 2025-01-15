72252beab19f1a1e1b3efadf84edb808cce32782
Mohammad Sahari <mohammadsahari98@outlook.com>
2025-01-15 18:15:15 +0330

    refactor : Hader Menu and Footer Are Moved To ViewComponents

---
+++ ServiceHost/ViewComponents/FooterViewComponent.cs
@@ -0,0 +1,13 @@
+﻿using _02_LampshadeQuery.Contract.ProductCategory;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ServiceHost.ViewComponents
+{
+    public class FooterViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
